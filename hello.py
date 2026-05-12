b5a5f87fd2793220141146354f319eb944185d08
Hakata <literallyme098@gmail.com>
2026-05-12 20:39:28 +0545

    practice

---
+++ hello.py
@@ -0,0 +1,16 @@
+def create_task(name,duration):
+    Task = {
+        "name" : name,
+        "duration" : duration
+    }
+
+    return Task
+
+def time_block(start_time,end_time):
+    Time = {
+        "start" : start_time,
+        "end" : end_time
+    }
+
+    return Time
+
